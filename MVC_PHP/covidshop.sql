ebe4e78ce7423145d2f34988ce7799c2f4c00f1e
szendeguz <63731570+szendeguz@users.noreply.github.com>
2020-11-20 18:39:47 +0100

    Update covidshop.sql

--- MVC_PHP/covidshop.sql
+++ MVC_PHP/covidshop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Gép: 127.0.0.1
--- Létrehozás ideje: 2020. Nov 19. 14:11
+-- Létrehozás ideje: 2020. Nov 20. 18:39
 -- Kiszolgáló verziója: 10.4.11-MariaDB
 -- PHP verzió: 7.4.6
 
@@ -72,6 +72,29 @@ INSERT INTO `customer` (`id`, `username`, `firstname`, `lastname`, `password`, `
 -- --------------------------------------------------------
 
 --
+-- Tábla szerkezet ehhez a táblához `orders`
+--
+
+CREATE TABLE `orders` (
+  `orderint` int(11) NOT NULL,
+  `userid` int(11) NOT NULL,
+  `items` varchar(500) NOT NULL,
+  `price` int(11) NOT NULL,
+  `date` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- A tábla adatainak kiíratása `orders`
+--
+
+INSERT INTO `orders` (`orderint`, `userid`, `items`, `price`, `date`) VALUES
+(1, 2, 'kesztyű 4, fertőtlenítő 2, lélegeztetőgép 3', 200000, '2020-11-20 16:44:17'),
+(2, 1, 'maszk 7, egyéb cuccos 3', 6450, '2020-11-20 17:17:57'),
+(3, 3, 'vírusteszt 1', 60000, '2020-11-20 17:18:53');
+
+-- --------------------------------------------------------
+
+--
 -- Tábla szerkezet ehhez a táblához `payment`
 --
 
@@ -104,6 +127,28 @@ CREATE TABLE `product` (
 INSERT INTO `product` (`id`, `itemName`, `price`, `quantity`, `description`, `picId`) VALUES
 (1, 'lélegeztetőgép', 300000, 1, 'szipiszupilélegeztetőgép', 'https://drive.google.com/uc?id=1c0-ie6crNSqFwqo-pwOgvfC2y1HIoTTp');
 
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- A tábla adatainak kiíratása `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `email`, `password`) VALUES
+(1, '1', '1@1.com', 'c4ca4238a0b923820dcc509a6f75849b'),
+(2, 'geo', 'geo@geo.com', 'ecc174e3e02c82f34c14fe860bf47ef2'),
+(3, 'guz_szende', 'szendi@szende.hu', '8b7f89be0bd7050edfde2b53fd6d5653');
+
 --
 -- Indexek a kiírt táblákhoz
 --
@@ -130,6 +175,13 @@ ALTER TABLE `customer`
   ADD UNIQUE KEY `username` (`username`);
 
 --
+-- A tábla indexei `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`orderint`),
+  ADD KEY `userid` (`userid`);
+
+--
 -- A tábla indexei `payment`
 --
 ALTER TABLE `payment`
@@ -141,6 +193,12 @@ ALTER TABLE `payment`
 -- A tábla indexei `product`
 --
 ALTER TABLE `product`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- A tábla indexei `users`
+--
+ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -166,6 +224,12 @@ ALTER TABLE `customer`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
+-- AUTO_INCREMENT a táblához `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `orderint` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- AUTO_INCREMENT a táblához `payment`
 --
 ALTER TABLE `payment`
@@ -178,6 +242,12 @@ ALTER TABLE `product`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
+-- AUTO_INCREMENT a táblához `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
 -- Megkötések a kiírt táblákhoz
 --
 
@@ -187,6 +257,12 @@ ALTER TABLE `product`
 ALTER TABLE `cartitem`
   ADD CONSTRAINT `cartitem_ibfk_1` FOREIGN KEY (`cartId`) REFERENCES `cart` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
   ADD CONSTRAINT `cartitem_ibfk_2` FOREIGN KEY (`productId`) REFERENCES `product` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+--
+-- Megkötések a táblához `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Megkötések a táblához `payment`
