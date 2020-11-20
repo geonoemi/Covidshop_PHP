4abb6f9bf186960b7008876df9d7d7f55e76160f
Noémi László <geonoemilaszlo@gmail.com>
2020-11-20 12:54:14 +0100

    working registry

--- MVC_PHP/controllers/registry.php
+++ MVC_PHP/controllers/registry.php
@@ -51,6 +51,6 @@ if (isset($_POST['reg_user'])) {
   	mysqli_query($db, $query);
   	$_SESSION['username'] = $username;
   	$_SESSION['success'] = "You are now logged in";
-  	header('location: index.php');
+  	header('Location: ../index.php');
   }
 }
--- MVC_PHP/users.sql
+++ MVC_PHP/users.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Gép: 127.0.0.1
--- Létrehozás ideje: 2020. Nov 20. 11:41
+-- Létrehozás ideje: 2020. Nov 20. 12:39
 -- Kiszolgáló verziója: 10.4.11-MariaDB
 -- PHP verzió: 7.4.6
 
@@ -33,6 +33,34 @@ CREATE TABLE `users` (
   `email` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- A tábla adatainak kiíratása `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `email`, `password`) VALUES
+(1, '1', '1@1.com', 'c4ca4238a0b923820dcc509a6f75849b'),
+(2, 'geo', 'geo@geo.com', 'ecc174e3e02c82f34c14fe860bf47ef2');
+
+--
+-- Indexek a kiírt táblákhoz
+--
+
+--
+-- A tábla indexei `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- A kiírt táblák AUTO_INCREMENT értéke
+--
+
+--
+-- AUTO_INCREMENT a táblához `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
