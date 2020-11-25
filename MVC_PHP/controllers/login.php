c0880ae08aad66beb0f5983a16dfc1766ad088db
szendeguz <63731570+szendeguz@users.noreply.github.com>
2020-11-25 19:06:00 +0100

    Update login.php

--- MVC_PHP/controllers/login.php
+++ MVC_PHP/controllers/login.php
@@ -1,18 +1,24 @@
 <?php
 //szar
 
-
 include 'views/login.php';
 if(isset($_POST['btnLogin'])){
-
     $checkUser = $MODEL->checkUserData($_POST['username'], $_POST['password']);
-  if($checkUser){
-        
-        $_SESSION["username"] = $_POST['username'];
-        echo $_SESSION["username"];
-        header("Location: index.php");
-        exit();
-
+    if($checkUser){
+        $isadmin = $MODEL->isAdmin($_POST['username']);
+        if($isadmin){
+            $set = "set";
+            $_SESSION["isadmin"] = $set;
+            $_SESSION["username"] = $_POST['username'];
+            header("Location: index.php");
+            exit();
+        }else{
+            $_SESSION["username"] = $_POST['username'];
+            $userid = $MODEL->getUserId($_POST["username"]);
+            $_SESSION["userid"] = $userid["id"];
+            header("Location: index.php");
+            exit();
+        }         
     }
 
 
