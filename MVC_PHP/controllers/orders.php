7e5a728cc449197ee0d3fcb63f56ace050e83ae1
szendeguz <63731570+szendeguz@users.noreply.github.com>
2020-11-25 19:08:10 +0100

    Update orders.php

--- MVC_PHP/controllers/orders.php
+++ MVC_PHP/controllers/orders.php
@@ -1,5 +1,7 @@
 <?php
 
-$VIEWDATA['allOrders'] = $MODEL->getAllOrders();
+if(isset($_SESSION['isadmin'])) $VIEWDATA['orders'] = $MODEL->getAllOrders();
+else $VIEWDATA['orders']=$MODEL->getOrdersByUsername($_SESSION['username']);
+
 
 include 'views/orders.php';
