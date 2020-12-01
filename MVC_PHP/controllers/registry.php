d129158271c38868627c4a71f46ced3b012d4e29
Noémi László <geonoemilaszlo@gmail.com>
2020-12-01 22:05:51 +0100

    password aaaand !=username

--- MVC_PHP/controllers/registry.php
+++ MVC_PHP/controllers/registry.php
@@ -2,14 +2,14 @@
 include 'views/registry.php';
 
 if (isset($_POST['reg_user'])) {
-  if($_POST["password_1"]===$_POST["password_2"]){
-    if (preg_match("#.*^(?=.{8,20})(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*\W).*$#",  $_POST['password_1'] )){
+  if($_POST["password_1"]===$_POST["password_2"] ){
+    if (preg_match("#.*^(?=.{8,20})(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*\W).*$#",  $_POST['password_1'] && $_POST["password_1"] !=$_POST["username"])){
 
     $MODEL->addUser($_POST["username"], $_POST["password_1"], $_POST["email"]);
     header("Location: index.php?c=login");
     exit();
     } else {
-      echo "<span class='error'> A jelszó legalább 8 karakter hosszú legyen, tartalmazzon legalább egy kisbetűt,egy nagybetűt és egy számot!</span>";
+      echo "<span class='error'> A jelszó ne egyezzen meg a felhasználónévvel, legalább 8 karakter hosszú legyen, tartalmazzon legalább egy kisbetűt,egy nagybetűt és egy számot!</span>";
     }
   }else echo "<span class='error'>A két jelszó nem egyezik.</span>";
 
