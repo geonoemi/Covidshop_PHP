2c3470901ffcd163aadc487dcea0aa34deb6cb32
Noémi László <geonoemilaszlo@gmail.com>
2020-12-01 21:52:48 +0100

    password regex

--- MVC_PHP/controllers/registry.php
+++ MVC_PHP/controllers/registry.php
@@ -1,13 +1,22 @@
 <?php
 include 'views/registry.php';
 
-  if (isset($_POST['reg_user'])) {
-    if($_POST["password_1"]===$_POST["password_2"]){
-  $MODEL->addUser($_POST["username"], $_POST["password_1"], $_POST["email"]);
-  header("Location: index.php?c=login");
-  exit();
+if (isset($_POST['reg_user'])) {
+  if($_POST["password_1"]===$_POST["password_2"]){
+    if (preg_match("#.*^(?=.{8,20})(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*\W).*$#",  $_POST['password_1'] )){
 
-  }
-  else echo "<span class='error'>A két jelszó nem egyezik.</span>";
+    $MODEL->addUser($_POST["username"], $_POST["password_1"], $_POST["email"]);
+    header("Location: index.php?c=login");
+    exit();
+    } else {
+      echo " A jelszó legalább 8 karakter hosszú legyen, tartalmazzon legalább egy kisbetűt,egy nagybetűt és egy számot!";
+    }
+  }else echo "<span class='error'>A két jelszó nem egyezik.</span>";
+
+
+  
 }
+    
+  
+
 ?>
