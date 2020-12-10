a785980ae33e8dcb155a869d04702009ec88b1d0
Emuzex <49400467+Emuzex@users.noreply.github.com>
2020-12-10 22:32:24 +0100

    a
    a

--- MVC_PHP/views/home.php
+++ MVC_PHP/views/home.php
@@ -7,6 +7,7 @@
         <title>Kezdolap</title>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
         <link rel="stylesheet" href="css/styles.css">
+        <link rel="stylesheet" href="css/interfaceupdate.css">
         <script src="js/cart.js"></script>
         
     </head>
